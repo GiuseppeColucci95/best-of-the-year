f137f5edf0d1173636b24cd902deae9a2b796c8f
GiuseppeColucci95 <colucci539@gmail.com>
2025-06-17 16:32:19 +0200

    add step 1

--- src/main/java/org/lessons/java/thymeleaf/best_of_the_year/controller/HomeController.java
+++ src/main/java/org/lessons/java/thymeleaf/best_of_the_year/controller/HomeController.java
@@ -1,5 +1,19 @@
 package org.lessons.java.thymeleaf.best_of_the_year.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
 public class HomeController {
 
+  @GetMapping("")
+  public String home(Model model) {
+
+    model.addAttribute("name", "Giuseppe");
+
+    return "home";
+  }
 }
